99310dffb77454b1b898097bad8eb7fffe0379ed
Georg Jaehnig <georg@jaehnig.org>
2017-05-16 16:10:34 +0200

    Concat.

--- test/test.js
+++ test/test.js
@@ -39,3 +39,24 @@ it('union', function() {
 	assert.equal(JSON.stringify(fsm1.Q), '[[{"1":{"0":{"0":1}},"2":{"1":{"1":1}}},0,0],[{},1,0],[{},1,0],[{"0":{"-1":{"-1":0.4}},"4":{"-1":{"-1":0.6}}},0,1]]'); 
 
 });
+
+
+it('concat', function() {
+
+	var fsm1 = new WFSM();
+	fsm1.setE( 0, 1, 0 );
+	fsm1.setE( 0, 2, 1 );
+	fsm1.setI( 0 );
+	fsm1.setF( 1 );
+	fsm1.setF( 2, 0.8 );
+
+	var fsm2 = new WFSM();
+	fsm2.setE( 0, 1, 2 );
+	fsm2.setI( 0, 0.5 );
+	fsm2.setF( 1 );
+
+	fsm1.concat( fsm2 );
+
+	assert.equal(JSON.stringify(fsm1.Q), '[[{"1":{"0":{"0":1}},"2":{"1":{"1":1}}},0,1],[{"3":{"-1":{"-1":0.5}}},0,0],[{"3":{"-1":{"-1":0.4}}},0,0]]'); 
+
+});
