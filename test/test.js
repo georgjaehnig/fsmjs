c9f8c90132a39ecf7679a87546d528cd961e30b5
Georg Jaehnig <georg@jaehnig.org>
2017-05-16 16:03:45 +0200

    Rename.

--- test/test.js
+++ test/test.js
@@ -5,16 +5,16 @@ var assert = require('assert');
 
 it('removeEpsilon', function() {
 
-	var fsm = new WFSM();
-	fsm.setE( 0, 1, 0, 0, 0.25 );
-	fsm.setE( 0, 1, fsm.EPS, fsm.EPS, 0.5 );
-	fsm.setE( 1, 1, fsm.EPS, fsm.EPS, 0.2 );
-	fsm.setE( 1, 0, 1, 1, 0.5 );
+	var wfsm = new WFSM();
+	wfsm.setE( 0, 1, 0, 0, 0.25 );
+	wfsm.setE( 0, 1, wfsm.EPS, wfsm.EPS, 0.5 );
+	wfsm.setE( 1, 1, wfsm.EPS, wfsm.EPS, 0.2 );
+	wfsm.setE( 1, 0, 1, 1, 0.5 );
 
-	fsm.setI( 1, 1 );
-	fsm.setF( 1, 0.3 );
-	fsm.setF( 0, 0.25 );
+	wfsm.setI( 1, 1 );
+	wfsm.setF( 1, 0.3 );
+	wfsm.setF( 0, 0.25 );
 
-	fsm.removeEpsilon();
-	assert.equal(JSON.stringify(fsm.Q), '[[{"0":{"1":{"1":0.3125}},"1":{"0":{"0":0.25},"-1":{}}},0.4375,0],[{"0":{"1":{"1":0.625}},"1":{"-1":{}}},0.375,1]]');
+	wfsm.removeEpsilon();
+	assert.equal(JSON.stringify(wfsm.Q), '[[{"0":{"1":{"1":0.3125}},"1":{"0":{"0":0.25},"-1":{}}},0.4375,0],[{"0":{"1":{"1":0.625}},"1":{"-1":{}}},0.375,1]]');
 });
