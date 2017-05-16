71e9da65d53f100cf2bb2039003e1bb3b086c47b
Georg Jaehnig <georg@jaehnig.org>
2017-05-16 16:08:00 +0200

    Add union.

--- test/test.js
+++ test/test.js
@@ -18,3 +18,24 @@ it('removeEpsilon', function() {
 	wfsm.removeEpsilon();
 	assert.equal(JSON.stringify(wfsm.Q), '[[{"0":{"1":{"1":0.3125}},"1":{"0":{"0":0.25},"-1":{}}},0.4375,0],[{"0":{"1":{"1":0.625}},"1":{"-1":{}}},0.375,1]]');
 });
+
+
+it('union', function() {
+
+	var fsm1 = new WFSM();
+	fsm1.setE( 0, 1, 0 );
+	fsm1.setE( 0, 2, 1 );
+	fsm1.setI( 0, 0.4 );
+	fsm1.setF( 1 );
+	fsm1.setF( 2 );
+
+	var fsm2 = new WFSM();
+	fsm2.setE( 0, 1, 2 );
+	fsm2.setI( 0, 0.6 );
+	fsm2.setF( 1 );
+
+	fsm1.union( fsm2 );
+
+	assert.equal(JSON.stringify(fsm1.Q), '[[{"1":{"0":{"0":1}},"2":{"1":{"1":1}}},0,0],[{},1,0],[{},1,0],[{"0":{"-1":{"-1":0.4}},"4":{"-1":{"-1":0.6}}},0,1]]'); 
+
+});
