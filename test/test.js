e9e6ce75601e1c330d9977e63c5fcd6d9c8b95a3
Georg Jaehnig <georg@jaehnig.org>
2017-05-16 16:00:22 +0200

    First working test.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
 'use strict'
 
 const WFSM = require('../src/index.js');
+var assert = require('assert');
 
-var wfsm = WFSM();
+it('removeEpsilon', function() {
 
-wfsm.setI( 0 );
-wfsm.setF( 0, 0.5 );
+	var fsm = new WFSM();
+	fsm.setE( 0, 1, 0, 0, 0.25 );
+	fsm.setE( 0, 1, fsm.EPS, fsm.EPS, 0.5 );
+	fsm.setE( 1, 1, fsm.EPS, fsm.EPS, 0.2 );
+	fsm.setE( 1, 0, 1, 1, 0.5 );
 
-wfsm.print();
+	fsm.setI( 1, 1 );
+	fsm.setF( 1, 0.3 );
+	fsm.setF( 0, 0.25 );
+
+	fsm.removeEpsilon();
+	assert.equal(JSON.stringify(fsm.Q), '[[{"0":{"1":{"1":0.3125}},"1":{"0":{"0":0.25},"-1":{}}},0.4375,0],[{"0":{"1":{"1":0.625}},"1":{"-1":{}}},0.375,1]]');
+});
